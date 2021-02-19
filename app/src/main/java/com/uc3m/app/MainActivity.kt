e21b095f179f0d555e8933cb51db4559813a9be2
María <100383440@alumnos.uc3m.es>
2021-02-19 09:54:04 +0100

    proyecto creado

---
+++ app/src/main/java/com/uc3m/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uc3m.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
